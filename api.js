99d160dfa771df3df4b211b0507d0f8621b223f3
Christian Fei <info@christian-fei.com>
2014-03-09 20:30:10 +0000

    format support

--- api.js
+++ api.js
@@ -1,15 +1,28 @@
 var speak = require('simple-tts'),
-    restify = require('restify');
+    restify = require('restify'),
+    url = require('url');
 
 var server = restify.createServer();
 
 server.get('/speak/:text', speakText);
 
 function speakText(req, res) {
-    res.writeHead(200, {'Content-Type': 'audio/ogg'});
-    speak(decodeURIComponent( req.params.text ), {format:'ogg', stream:res});
+    var query = url.parse( req.url, true ).query;
+
+    var options = {
+        stream: res,
+        format: "ogg"
+    }
+    
+    if( query &&  query.format == "mp3" ){
+        options.format = "mp3";
+        res.writeHead(200, {'Content-Type': 'audio/mpeg'});
+    }else{
+        res.writeHead(200, {'Content-Type': 'audio/ogg'});
+    }
+    speak(decodeURIComponent( req.params.text ), {stream: res, format:"ogg"});
 }
 
 server.listen(3040, function() {
-      console.log('%s listening at %s', server.name, server.url);
+    console.log('%s listening at %s', server.name, server.url);
 });
